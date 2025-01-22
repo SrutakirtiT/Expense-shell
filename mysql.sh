132fb84033093d40c6aee9543454bc7c2d93f906
sruthakeerthi <sruthakeerthi53@gmail.com>
2025-01-22 23:16:25 +0800

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -6,13 +6,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/e-logs"
+LOGS_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2 ... $R FAILURE $N"
         exit1
@@ -22,7 +22,7 @@ VALIDATE(){
 }
 
 CHECK_ROOT(){
-    if [ $USERID -ne 0]
+    if [ $USERID -ne 0 ]
     then
         echo "ERROR:: You must have sudo access to execute this script"
         exit 1 #other than 0
