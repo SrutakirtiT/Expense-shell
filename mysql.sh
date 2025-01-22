07132bc7505c5d66d463c2f84722efb0d203a4f6
sruthakeerthi <sruthakeerthi53@gmail.com>
2025-01-22 23:38:28 +0800

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -46,10 +46,11 @@ mysql_secure_installation --set-root-pass ExpenseApp@1
 VALIDATE $? "Setting root password"
 
 mysql -h mysql.tskdaws.online -u root -pExpenseApp@1 -e 'show databases;' &>>LOG_FILE_NAME
+ 
   if [ $? -ne 0 ]
   then
       echo "MySQL root password not set up" &>>$LOG_FILE_NAME
-      mysql-secure-installlation --set-root-passExpenseApp@1
+      mysql-secure-installlation --set-root-pass ExpenseApp@1
       VALIDATE $? "Setting root password"
 
   else
